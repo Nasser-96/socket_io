1ba2ac919d80bc179bcbb667c6a6695697a8146f
Nasser Abunasir <79524536+Nasser-96@users.noreply.github.com>
2024-01-15 20:04:28 +0300

    create login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) 
+{
+  const router = useRouter()
+
+  useEffect(()=>
+  {
+    const token = localStorage.getItem('token');
+    if(!token)
+    {
+      router.push('/login')
+    }
+  },[])
+
   return (
     <main className='bg-slate-900 h-full min-h-screen'>
       <Component {...pageProps} />
